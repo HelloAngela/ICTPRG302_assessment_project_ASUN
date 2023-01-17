a5f9b06e3d8c8622d5f676ca9dd8e3ba2a9b3700
helloAngela <hellongla@hotmail.com>
2023-01-17 11:12:39 +1100

    ICTPRG302 assessment project

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+
+# Welcome message
+print('Welcome to Gelos Enterprise! What would you like to do today?')
+
+# Ask user to select function
+task = int(input("Enter numbers 1~4 to access the corresponding function:\n 1. Login \n 2. Register for a new account\n"
+              " 3. View accounts (Administrator only) \n 4. Exit"))
+
+if task == 1:
+    print ("Login")
+    uid = input("Please enter your user name:")
+    pw = input("Please provide your password:")
+    with open a
+
+elif task == 2:
+    rrr
+elif task == 3:
+    rrr
+elif task == 4:
+    rrr
+else:
+    print ("No function associated with this input, please enter numbers 1~4 only")
+
+
